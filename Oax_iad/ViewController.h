6ca734a5fae5edb7e2384103104ad2d3703deb54
jvlioverne <jvlioverne@gmail.com>
2015-02-13 14:15:35 -0600

    Como agregar un banner de publicidad iAD

--- Oax_iad/ViewController.h
+++ Oax_iad/ViewController.h
@@ -7,9 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <iAd/iAd.h>
 
-@interface ViewController : UIViewController
-
+@interface ViewController : UIViewController <UIApplicationDelegate,ADBannerViewDelegate>
+{
+    ADBannerView *adView;
+    BOOL bannerIsVisible;
+}
 
 @end
 

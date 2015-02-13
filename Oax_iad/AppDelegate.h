73e12769c10ffd71cffabacab9d6f74d8850a745
jvlioverne <jvlioverne@gmail.com>
2015-02-12 19:52:47 -0600

    Initial Commit

---
+++ Oax_iad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Oax_iad
+//
+//  Created by Benjas on 12/02/15.
+//  Copyright (c) 2015 Benjas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
